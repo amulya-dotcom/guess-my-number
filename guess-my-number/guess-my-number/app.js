32fe9e532cdbee900676da3742040529818cb375
Amulya <74760148+amulya-dotcom@users.noreply.github.com>
2022-09-02 23:43:24 +0530

    Add files via upload

---
+++ guess-my-number/guess-my-number/app.js
@@ -0,0 +1,6 @@
+let randomNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector(".number").textContent = randomNumber;
+
+document.querySelector(".check").addEventListener("click", () => {
+  let guess = querySelector(".check");
+});
